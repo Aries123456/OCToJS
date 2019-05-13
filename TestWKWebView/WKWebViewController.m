647d93497dfb3e092e32eb34fbcb32de18a7b125
lk <348994901@qq.com>
2019-05-13 22:09:01 +0800

    1

--- TestWKWebView/WKWebViewController.m
+++ TestWKWebView/WKWebViewController.m
@@ -30,18 +30,36 @@
 {
     //创建网页配置对象
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    // 是使用h5的视频播放器在线播放, 还是使用原生播放器全屏播放
+    config.allowsInlineMediaPlayback = YES;
+    //设置视频是否需要用户手动播放  设置为NO则会允许自动播放
+    config.requiresUserActionForMediaPlayback = YES;
+    //设置是否允许画中画技术 在特定设备上有效
+    config.allowsPictureInPictureMediaPlayback = YES;
+    //设置请求的User-Agent信息中应用程序名称 iOS9后可用
+    config.applicationNameForUserAgent = @"ChinaDailyForiPad";
     //通过JS与webView内容交互
     config.userContentController = [WKUserContentController new];
     [config.userContentController addScriptMessageHandler:self name:@"Back"];
     [config.userContentController addScriptMessageHandler:self name:@"Color"];
     [config.userContentController addScriptMessageHandler:self name:@"Param"];
     
+    // 创建设置对象
+    WKPreferences *preference = [[WKPreferences alloc]init];
+    //最小字体大小 当将javaScriptEnabled属性设置为NO时，可以看到明显的效果
+    preference.minimumFontSize = 0;
+    //设置是否支持javaScript 默认是支持的
+    preference.javaScriptEnabled = YES;
+    // 在iOS上默认为NO，表示是否允许不经过用户交互由javaScript自动打开窗口
+    preference.javaScriptCanOpenWindowsAutomatically = YES;
+    config.preferences = preference;
+    
+    
+    
 //    [config.userContentController removeScriptMessageHandlerForName:@"Back"];
     
     self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 300) configuration:config];
-    // UI代理
     self.webView.UIDelegate = self;
-    // 导航代理
     self.webView.navigationDelegate = self;
     NSString *path = [[NSBundle mainBundle] pathForResource:@"WKWebViewText.html" ofType:nil];
     NSString *htmlString = [[NSString alloc]initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
